1ffc4901964c103de2be49a1f6eaaf122f46f1cd
Pao <chaiwatt77@gmail.com>
2024-01-11 15:42:10 +0700

    ex:1-7

--- exercise-1.js
+++ exercise-1.js
@@ -1,1 +1,21 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+    { $group: { _id: "$credit_card_type", total_paid: { $sum: "$total_price" } } },
+    ]);
+
+/*
+db.pizzaOrders.aggregate([
+{
+$group: {
+_id: "$credit_card_type",
+total_paid: { $sum: "$total_price" }
+}
+},
+{
+$group: {
+_id: null,
+totalprice: { $sum: "$total_paid" }
+}
+}
+]);``
+*/
--- exercise-4.js
+++ exercise-4.js
@@ -1,1 +1,12 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+    {
+    $group: {
+    _id: {
+    month: "$created_at",
+    year: "$created_at",
+    },
+    total_sales: { $sum: "$total_price" },
+    },
+    },
+    ])
