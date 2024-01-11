10228e279fa8d3c8ee170da25b7c6b072f974fce
suphattraying <suphattra.spk@gmail.com>
2024-01-11 15:31:59 +0700

    finish

--- exercise-4.js
+++ exercise-4.js
@@ -1,1 +1,5 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+    { $group : {_id : {$year : "$created_at"} , totalSalesPerYear : {$sum : "$total_price" }}},
+    { $sort : {_id : -1}}
+    ])
